dd58d6a79b3c0b3d8de26e4da0eab7f7e038ceb7
Trần Huy Linh <l_tran@vision-net.co.jp>
2026-01-17 10:05:13 +0700

    update

--- script.js
+++ script.js
@@ -240,7 +240,7 @@ function checkSynstaxText(arr) {
     const textError = arr.filter(item => {
         const newItem = item.toLowerCase();
         const regex = /\w+1$/;
-        return regex.test(newItem) || newItem.includes('#') || newItem.includes('không định hình được');
+        return regex.test(newItem) || newItem.includes('#') || newItem.includes('không được định hình');
     });
     const result = document.querySelector('.check-syntax-error');
     if (result) {
